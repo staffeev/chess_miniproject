ff9300a1d57f0cc08f4af7feef30d44f67dd774b
staffeev <st.affeev@yandex.ru>
2024-05-07 11:29:14 +0300

    xvzxv

--- gui/canvas_widget.py
+++ gui/canvas_widget.py
@@ -3,11 +3,15 @@ from PyQt5.QtSvg import QGraphicsSvgItem
 import os
 
 
+CANVAS_SIZE = (480, 480)
+
+
 class Canvas(QMainWindow):
     def __init__(self, board):
         super().__init__()
         self.board = board
         self.scene = QGraphicsScene()
+        self.scene.setSceneRect(0, 0, *CANVAS_SIZE)
         self.view = QGraphicsView()
         self.view.setScene(self.scene)
         self.setCentralWidget(self.view)
@@ -15,12 +19,17 @@ class Canvas(QMainWindow):
     @staticmethod
     def get_svg_filename(elem):
         """Получение имени файла с изображением фигуры"""
-        filename = f"{elem.__class__.__name__.lower()}_{elem.color}.svg"
+        if isinstance(elem, str):
+            filename = f"{elem}.svg"
+        else:
+            filename = f"{elem.__class__.__name__.lower()}_{elem.color}.svg"
         path = os.path.join(os.path.dirname(__file__), "svg_icons", filename)
         return path
     
     def draw(self):
         """Отрисовка элементов доски"""
+        item = QGraphicsSvgItem(self.get_svg_filename("chessboard"))
+        self.scene.addItem(item)
         for i in self.board.field:
             item = QGraphicsSvgItem(self.get_svg_filename(i))
             item.setFlag(QGraphicsItem.ItemIsMovable)
